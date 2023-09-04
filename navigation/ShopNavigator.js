0c8791791b40139538ba51f0b0f7970ca62ecb03
Harsh Mishra <50140154+Harsh2110mishra@users.noreply.github.com>
2023-09-04 21:52:32 +0530

    Update expo sdk (#2)
    * Fix[Update]: sdk 47 & some bug fixes
    
    * v1: bug fixes
    
    * fix: update issues
    
    ---------
    
    Co-authored-by: Harsh Mishra <harshmishra@Harshs-MacBook-Air.local>

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -130,7 +130,7 @@ const ShopNavigator = createDrawerNavigator(
               color={Colors.primary}
               onPress={() => {
                 dispatch(authActions.logout());
-                // props.navigation.navigate("Auth");
+                props.navigation.navigate("Auth");
               }}
             />
           </SafeAreaView>
